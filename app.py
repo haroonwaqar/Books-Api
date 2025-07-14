0d5e979afcab642eee2712e472213913a81b046d
Haroon Waqar <haroonwaqar1234@gmail.com>
2025-07-14 18:12:49 +0500

    Initial commit: Flask REST API with PostgreSQl

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from model import Book
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/RestApiData'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    book = Book(title=data["title"], author=data["author"])
+    db.session.add(book)
+    db.session.commit()
+    return ('Book is added'), 201
+
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+    return jsonify([book.to_dict() for book in books])
+
+
+@app.route('/books/<int:id>')
+def get_a_book(id):
+    book = Book.query.filter_by(id=id).first()
+    if book:
+        return jsonify(book.to_dict())
+    else:
+        return ('Book not found', 404)
+    
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    data = request.get_json()
+    book = Book.query.filter_by(id=id).first()
+    if book:
+        book.title = data["title"]
+        book.author = data["author"]
+        db.session.commit()
+        return jsonify(book.to_dict())
+    else:
+        return ('Book not found', 404)
+    
+
+@app.route('/books/<int:id>', methods=['PATCH'])
+def patch_book(id):
+    data = request.get_json()
+    book = Book.query.filter_by(id=id).first()
+    if book:
+        if "title" in data:
+            book.title = data["title"]
+        if "author" in data:
+            book.author = data["author"]
+        db.session.commit()
+        return jsonify(book.to_dict())
+    else:
+        return ('Book not found', 404)
+    
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.filter_by(id=id).first()
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+        return ('The book has been deleted', 200)
+    else:
+        return ('Book not found', 404)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() 
+    app.run(debug=True)
+
